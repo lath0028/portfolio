d014fe219334533ec0affeb09dba23333f425498
Prashant-lathiya <32398066+Prashant-lathiya@users.noreply.github.com>
2019-11-20 18:00:41 -0500

    Updates

---
+++ precache-manifest.764a32a6d7f6a210c28a08865e8c634c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5203c07cbff467ddd7de48d339df746",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8684eb4d620cb78d5029",
+    "url": "/portfolio/static/css/main.757fdb17.chunk.css"
+  },
+  {
+    "revision": "d499d87a993599493776",
+    "url": "/portfolio/static/js/2.1e4a2dbe.chunk.js"
+  },
+  {
+    "revision": "8684eb4d620cb78d5029",
+    "url": "/portfolio/static/js/main.725790fd.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "e20c226d5d7e3ebfc806996646c894bc",
+    "url": "/portfolio/static/media/CareMate_Final_New.e20c226d.mp4"
+  },
+  {
+    "revision": "319566e90502e4d9e94af7272e17a088",
+    "url": "/portfolio/static/media/SpeechRecognizerMASV.319566e9.mp4"
+  },
+  {
+    "revision": "c764a440a8bdde10d11e44411b76c426",
+    "url": "/portfolio/static/media/email.c764a440.svg"
+  },
+  {
+    "revision": "d1761ff7e0c48f95e31bbf9c8928e9cc",
+    "url": "/portfolio/static/media/github.d1761ff7.svg"
+  },
+  {
+    "revision": "f0fa873b0f1628b4ebda9f84f837a19a",
+    "url": "/portfolio/static/media/linkedin.f0fa873b.svg"
+  },
+  {
+    "revision": "fdcbc741d4de62e98dd4d090b8b878bb",
+    "url": "/portfolio/static/media/passport_application.fdcbc741.mov"
+  },
+  {
+    "revision": "dff99854b15f80d4cd8d007bd93cd51a",
+    "url": "/portfolio/static/media/phone.dff99854.svg"
+  }
+]);
